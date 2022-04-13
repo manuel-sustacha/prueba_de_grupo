df31d2c907ffefe9693347506a1ba3a4d93c9076
WILDER BORIS LOPEZ <wbl_0904@hotmail.com>
2022-04-13 12:56:56 -0400

    añadi db  mas codigo para la conexion de la base de datos

---
+++ db.php
@@ -0,0 +1,6 @@
+<?php
+
+$conexion=mysqli_connect("localhost","root","","validar");
+
+?>
+<H1>CONECTADO</H1>
---
+++ home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+</head>
+<body>
+<h1>BAZAR L & N</h1>
+<h2>BIENVENIDOS</h2>
+    
+</body>
+</html>
